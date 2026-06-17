058c3eff7b82cac57159ef19277bbbf1cb66a2eb
sundhar-1432 <SVSsundhar143@gmail.com>
2026-06-17 19:46:19 +0530

    debuging the secretkey

--- deply_pro/settings.py
+++ deply_pro/settings.py
@@ -27,7 +27,10 @@ STORAGES = {
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config(
+    "SECRET_KEY",
+    default="temporary-secret-key"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
